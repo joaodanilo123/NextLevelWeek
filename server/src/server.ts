687139f929222c7132497038657bd730d807d9c9
joaodanilo123 <jddiedrich@gmail.com>
2020-06-02 22:26:32 -0300

    Dia 2: backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,18 @@
 import express from 'express';
+import routes from './routes';
+import path from 'path';
+import cors from 'cors';
 
 const app = express();
-
-app.get('/users', (req, res)=>{
-    res.json(['Diego', 'Cleiton'])
-})
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 const port = 3333;
 
-app.listen(port, ()=>{
-    console.log(`Litening to port ${3333}`)
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.listen(port, () => {
+    console.clear();
+    console.log(`Litening to port ${port}`)
 })
